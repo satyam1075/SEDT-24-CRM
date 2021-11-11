629d665795acba97e845cc3ad4f96d2f7e08a011
satyam <satyam@MSI>
2021-11-11 12:41:41 +0530

    one new class

---
+++ ResumeProject/src/test/java/TestScripts/GitShare.java
@@ -0,0 +1,12 @@
+package TestScripts;
+
+import org.testng.annotations.Test;
+
+public class GitShare 
+{
+	@Test
+	public void share() 
+	{
+		System.out.println("pushi my first edited file");
+	}
+}
